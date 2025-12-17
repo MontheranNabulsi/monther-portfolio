424d16c045acf325db5876c05f708a9d5e2ac556
Monther Nabulsi <m.nabulsi00@gmail.com>
2025-12-17 12:52:41 +0300

    added projects.ts

--- src/app/models/project.ts
+++ src/app/models/project.ts
@@ -1,2 +1,14 @@
 export interface Project {
+  id: number;
+  title: string;
+  category: string;
+  description: string;
+  image: string;
+  technologies: string[];
+  additionalTechs: number;
+  period: string;
+  githubLink: string;
+  liveLink: string;
+  contributions: string[];
+  techStack: string[];
 }
